3d7a94d238abce6b3c866259707d380c38670c93
SHAMSBIN HARUN <sbharun@myseneca.ca>
2024-09-25 12:38:02 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Author: Shams Bin Harun
+# Author ID: sbharun
+# Date Created: 2024/09/25
+
+import sys
+
+# Check if an argument is provided
+if len(sys.argv) != 2:
+    print("Usage: ./lab2f.py <number>")
+    sys.exit(1)
+
+try:
+    timer = int(sys.argv[1])
+except ValueError:
+    print("The argument must be an integer.")
+    sys.exit(1)
+
+# Loop until timer reaches 0
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
+
