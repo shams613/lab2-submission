5531d79369588056965f2e0d8293b7f2cc7024c4
SHAMSBIN HARUN <sbharun@myseneca.ca>
2024-09-25 12:28:41 -0400

    Added lab2d

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Shams Bin Harun
+# Author ID: sbharun
+# Date Created: 2024/09/25
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
